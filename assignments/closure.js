bb482172cc4fa5a688c23e7b560efb73b63af5d1
Denvinn Magsino <denvinnmagsino@Denvinns-MacBook-Air.local>
2019-05-25 12:38:38 -0700

    finished stretched goals

--- assignments/closure.js
+++ assignments/closure.js
@@ -22,14 +22,27 @@ const government = () =>{
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
   // Return a function that when invoked increments and returns a counter variable.
+  var counter=0;
+ return ()=>{
+    return ++counter;
+  }
 };
-// Example usage: const newCounter = counter();
-// newCounter(); // 1
-// newCounter(); // 2
+
+const newCounter = counter();
+console.log(newCounter()); // 1
+console.log(newCounter());
+
 
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
-const counterFactory = () => {
+const counterFactory = (num) => {
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
+  var indecrement={
+  increment: counter =>{return ++counter;},
+  decrement: counter =>{return --counter;}
+}
+console.log(indecrement.decrement(num));
 };
+
+counterFactory(50);
